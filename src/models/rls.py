c456b225c658d3cedc7771fa6e34134f4ed8ebba
Artem Lobantsev <armavox@gmail.com>
2020-05-04 12:58:43 +0300

    :construction: rls works

--- src/models/rls.py
+++ src/models/rls.py
@@ -1,5 +1,7 @@
 from functools import partial
 
+import matplotlib.pyplot as plt
+
 import numpy as np
 import raster_geometry
 import torch
@@ -32,27 +34,38 @@ class RLSModule(nn.Module):
         super().__init__()
 
         self.img_size = input_size[-2:]  # [H, W]
-        in_feat = np.prod(self.img_size)
+        in_feat = self.img_size
 
-        self.ug = nn.Parameter(torch.zeros(in_feat, in_feat).normal_(std=0.01))
-        self.wg = nn.Parameter(torch.zeros(in_feat, in_feat).normal_(std=0.01))
+        self.ug = nn.Parameter(torch.zeros(*in_feat).normal_(std=0.01))
+        self.wg = nn.Parameter(torch.zeros(*in_feat).normal_(std=0.01))
         nn.init.eye_(self.ug)
         nn.init.eye_(self.wg)
 
-        # self.uzr = nn.Parameter(torch.zeros(2 * in_feat, in_feat).normal_(std=0.01))
-        # self.wzr = nn.Parameter(torch.zeros(2 * in_feat, in_feat).normal_(std=0.01))
-        # self.bz = nn.Parameter(torch.zeros(2 * in_feat))
-        # self.uo = nn.Parameter(torch.zeros(in_feat, in_feat).normal_(std=0.01))
-        # self.wo = nn.Parameter(torch.zeros(in_feat, in_feat).normal_(std=0.01))
-        # self.bo = nn.Parameter(torch.zeros(in_feat))
+        self.uz = nn.Parameter(torch.zeros(*in_feat).normal_(std=0.01))
+        self.wz = nn.Parameter(torch.zeros(*in_feat).normal_(std=0.01))
+        self.bz = nn.Parameter(torch.zeros(in_feat[0]))
+        nn.init.eye_(self.uz)
+        nn.init.eye_(self.wz)
 
-        self.gru = nn.GRUCell(in_feat, in_feat)
-        self.dense = nn.Linear(in_feat, in_feat)
+        self.ur = nn.Parameter(torch.zeros(*in_feat).normal_(std=0.01))
+        self.wr = nn.Parameter(torch.zeros(*in_feat).normal_(std=0.01))
+        self.br = nn.Parameter(torch.zeros(in_feat[0]))
+        nn.init.eye_(self.ur)
+        nn.init.eye_(self.wr)
 
-        nn.init.eye_(self.gru.weight_hh)
-        nn.init.eye_(self.gru.weight_ih)
-        self.gru.bias_ih.data.zero_()
-        self.gru.bias_hh.data.zero_()
+        self.uo = nn.Parameter(torch.zeros(*in_feat).normal_(std=0.01))
+        self.wo = nn.Parameter(torch.zeros(*in_feat).normal_(std=0.01))
+        self.bo = nn.Parameter(torch.zeros(in_feat[0]))
+        nn.init.eye_(self.uo)
+        nn.init.eye_(self.wo)
+
+        # self.gru = nn.GRUCell(*in_feat)
+        self.dense = nn.Linear(*in_feat)
+
+        # nn.init.eye_(self.gru.weight_hh)
+        # nn.init.eye_(self.gru.weight_ih)
+        # self.gru.bias_ih.data.zero_()
+        # self.gru.bias_hh.data.zero_()
 
         nn.init.xavier_normal_(self.dense.weight)
         self.dense.bias.data.zero_()
@@ -65,22 +78,22 @@ class RLSModule(nn.Module):
         batch_size = input.size(0)
 
         c1, c2 = self.avg_inside(input, hidden.detach()), self.avg_outside(input, hidden.detach())
-        I_c1 = ((input - c1) ** 2).view(batch_size, -1)
-        I_c2 = ((input - c2) ** 2).view(batch_size, -1)
-        kappa = self.curvature(hidden).view(batch_size, -1)
+        I_c1 = (input - c1) ** 2
+        I_c2 = (input - c2) ** 2
+        kappa = self.curvature(hidden)
         make_grid_p = partial(make_grid, nrow=4, normalize=True)
         if writer is not None:
-            writer.add_image(f"rls/hidden", make_grid_p(hidden.detach().view(-1, 1, 64, 64)), step)
-            writer.add_image(f"rls/I_c1", make_grid_p(I_c1.detach().view(-1, 1, 64, 64)), step)
-            writer.add_image(f"rls/I_c2", make_grid_p(I_c2.detach().view(-1, 1, 64, 64)), step)
-            writer.add_image(f"rls/kappa", make_grid(kappa.detach().view(-1, 1, 64, 64), 4), step)
+            writer.add_image(f"rls/hidden", make_grid_p(hidden.detach()), step)
+            writer.add_image(f"rls/I_c1", make_grid_p(I_c1.detach()), step)
+            writer.add_image(f"rls/I_c2", make_grid_p(I_c2.detach()), step)
+            writer.add_image(f"rls/kappa", make_grid(kappa.detach(), 4), step)
 
-        x = kappa - torch.mm(I_c1, self.ug) + torch.mm(I_c2, self.wg)
-        # hidden = gru_rls_cell(
-        #   x, hidden.view(batch_size, -1), self.uzr, self.wzr, self.bz, self.uo, self.wo, self.bo
-        # )
-        hidden = self.gru(x, hidden.view(batch_size, -1))
-        hidden = F.relu(hidden)
+        hidden = self.gru_rls_cell(hidden, kappa, I_c1, I_c2)
+        plot(input, 'input')
+        plot(hidden, "levelset")
+        plot(kappa, "kappa")
+        # hidden = self.gru(x, hidden.view(batch_size, -1))
+        # hidden = F.relu(hidden)
         output = self.dense(hidden)
         return (
             output.view(batch_size, *self.img_size),
@@ -91,15 +104,10 @@ class RLSModule(nn.Module):
         """hidden.size: [B, C, H, W], C == 1"""
 
         phi_dx = img_derivative(hidden, SOBEL_X)
-        phi_dxx = img_derivative(phi_dx, SOBEL_X)
         phi_dy = img_derivative(hidden, SOBEL_Y)
-        phi_dyy = img_derivative(phi_dy, SOBEL_Y)
-        phi_dxy = img_derivative(phi_dx, SOBEL_Y)
-
-        kappa_nom = (phi_dxx * phi_dy ** 2 - 2 * phi_dx * phi_dy * phi_dxy + phi_dyy * phi_dx ** 2)
-        kappa_denom = (phi_dx ** 2 + phi_dy ** 2) ** (3 / 2) + 1e-8
-        kappa = kappa_nom / kappa_denom
-        return kappa
+        phi_dx_n = phi_dx / (phi_dx ** 2 + phi_dy ** 2 + 1e-8) ** 0.5
+        phi_dy_n = phi_dy / (phi_dx ** 2 + phi_dy ** 2 + 1e-8) ** 0.5
+        return -(torch.abs(phi_dx_n) + torch.abs(phi_dy_n))
 
     def avg_inside(self, input, level_set):
         mask = heaviside(level_set) > 0.5
@@ -110,6 +118,16 @@ class RLSModule(nn.Module):
         mask = heaviside(level_set) > 0.5
         outside = input * ~mask
         return outside.mean()
+
+    def gru_rls_cell(self, hidden, kappa, I_c1, I_c2):
+
+        x = kappa + self.ug @ I_c1 - self.wg @ I_c2
+        plot(x, "x")
+        z_t = F.sigmoid(self.uz @ x + self.wz @ hidden + self.bz)
+        r_t = F.sigmoid(self.ur @ x + self.wr @ hidden + self.br)
+        o_t = F.tanh(self.uo @ x + self.wo @ (hidden * r_t) + self.bo)
+
+        return z_t * hidden + (1 - z_t) * o_t
 
 
 def init_levelset(img_size: tuple, shape: str = "checkerboard"):
@@ -133,3 +151,8 @@ def init_levelset(img_size: tuple, shape: str = "checkerboard"):
 # plt.colorbar()
 # plt.savefig('hidden.png')
 # plt.close()
+
+
+def plot(matrix, name):
+    plt.imshow(matrix[0][0], cmap="gray")
+    plt.savefig(f"{name}.png")
